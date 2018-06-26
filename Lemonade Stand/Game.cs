45738e847723d09e820a386cb6a6bcea5b932ffb
danielfernandez63 <danmanfernan@gmail.com>
2018-06-26 11:29:32 -0500

    query added into the order

--- Lemonade Stand/Game.cs
+++ Lemonade Stand/Game.cs
@@ -2,19 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading.Tasks; 
 
 namespace Lemonade_Stand
 {
     class Game
     {
-        Weather weather = new Weather();
+        Day day = new Day();
 
 
         public void RunGame()
         {
             GameDirections();
-            StartGamePrompt();
+            StartDayPrompt();
             
         }
 
@@ -25,19 +25,103 @@ namespace Lemonade_Stand
 
         }
 
-        public void StartGamePrompt()
+        public void StartDayPrompt()
         {
-            Console.WriteLine(" Here are some starting details for the day. ");
-            weather.GetForecast();
+            
+            Console.WriteLine(" Here are some starting details for the day use this to follow through. ");
+            day.GetForecast();
             Console.ReadLine();
-            Console.WriteLine("Current inventory is" );
-            Console.WriteLine("");
-
-
-
+            // Console.WriteLine("Current inventory is");
+            //display inventory
+            //Console.ReadLine();
+            PurchaseMaterials();
         }
 
 
+        public void PurchaseMaterials()
+        {
+            string response;
+            Console.WriteLine("Would you like to purchase more materials. 'yes' 'no' before you start for the day. ");
+            response = Console.ReadLine().ToLower().Trim();
+
+            if (response == "yes")
+            {
+                //update materials and from the end of that go to change recipe
+
+
+            }
+            else if (response == "no")
+            {
+                SetRecipe();
+
+            }
+            else 
+            {
+            
+                Console.WriteLine("Please follow directions and enter a valid input");
+                PurchaseMaterials();
+
+            }
+
+        }
+
+        public void SetRecipe()
+        {
+            string response;
+            //display current recipe
+
+            Console.WriteLine("Would you like to change  your recipe before you start for the day. 'yes' 'no' ");
+            response = Console.ReadLine().ToLower().Trim();
+
+            if (response == "yes")
+            {
+                //update recipe and from there continue
+
+
+            }
+            else if (response == "no")
+            {
+
+                SetPrice();
+            }
+            else
+            {
+
+                Console.WriteLine("Please follow directions and enter a valid input");
+                SetRecipe();
+
+            }
+
+        }
+            public void SetPrice()
+            {
+                string response;
+                //display current price
+
+                Console.WriteLine("Would you like to change  your set price of the product before you start for the day. 'yes' 'no' ");
+                response = Console.ReadLine().ToLower().Trim();
+
+                if (response == "yes")
+                {
+                    //update price and from there continue
+
+
+                }
+                else if (response == "no")
+                {
+                   //continue 
+
+                }
+                else
+                {
+
+                    Console.WriteLine("Please follow directions and enter a valid input");
+                    SetPrice();
+
+                }
+
+
+            }
 
 
 
