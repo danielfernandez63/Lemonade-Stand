b0d0b556f696426792611d979d6b72ac5604a04d
danielfernandez63 <danmanfernan@gmail.com>
2018-06-28 15:11:06 -0500

    store hashed out with all items

--- Lemonade Stand/Game.cs
+++ Lemonade Stand/Game.cs
@@ -22,20 +22,35 @@ namespace Lemonade_Stand
         {
             GameDirections();
             StartDayPrompt();
-
         }
 
         public void GameDirections()
         {
-          Console.WriteLine("Welcome to the lemonade stand game. The object here is to make the maximum amount of money possible in the game... etc");
+          Console.WriteLine("");
+          Console.WriteLine("");
+          Console.WriteLine("");
+          Console.WriteLine("Welcome to the lemonade stand game. ");
+          Console.WriteLine("");
+          Console.WriteLine("The object here is to make the maximum amount of money in this game. You will " +
+              "start out with $20.00 and will have to purchase your ingredients at the store. Make sure to make smart choices in buying enough" +
+              " ingredients to make enough what you need to sell but not too much and waste money. Your recipe you create will be a factor in sales" +
+              " so change your recipe to boost sales. Weather will be forecasted but maybe be different during the day and will alter sales as well. " +
+              " Sale price will also be a factor in making sales. Use these factors to plan ahead and try and boost sales accordingly. ");
+          Console.WriteLine("");
+          Console.WriteLine("GOOD LUCK");
           Console.ReadLine();
+          Console.Clear();
         }
+
+      
 
         public  void StartDayPrompt()
         {
 
-            Console.WriteLine(" Here are some starting details for the day use this to follow through. ");
+            Console.WriteLine(" Here are you starting details: ");
             Day day = new Day();
+            Console.WriteLine("");
+            Console.WriteLine("");
             Console.WriteLine("");
             day.GetDailyForecast();
             Console.WriteLine("");
@@ -77,6 +92,9 @@ namespace Lemonade_Stand
         public  void SetRecipe()
         {
             string response;
+            Console.Clear();
+            player.GetInventory();
+            //Day.GetDailyForecast();
             //display current recipe
 
             Console.WriteLine("Would you like to change  your recipe before you start for the day. 'yes' 'no' ");
--- Lemonade Stand/Store.cs
+++ Lemonade Stand/Store.cs
@@ -8,22 +8,19 @@ namespace Lemonade_Stand
 {
     class Store
     {
-        
-        
+           
 
         public Store()
         {
-            //this.player = player;
-
-            //Console.WriteLine("you have " + player.Lemons);
-
-            
 
         }
         
         public void StartShopping(Player player)
         {
             string response;
+            Console.Clear();
+            Console.WriteLine("");
+            player.GetInventory();
             Console.WriteLine("");
             Console.WriteLine("Welcome to Dan's Mega Store Emporium. What would you like to buy? Options are 'lemons', 'ice', 'cups', sugar' or 'done'.");
             response = Console.ReadLine().ToLower().Trim();
@@ -36,25 +33,25 @@ namespace Lemonade_Stand
 
             else if (response == "ice")
             {
-
+                BuyIce(player);
 
             }
 
             else if (response == "cups")
             {
-
+                BuyCups(player);
 
             }
 
             else if (response == "sugar")
             {
-
+                BuySugar(player);
 
             }
 
             else if (response == "done")
             {
-
+                
 
             }
 
@@ -71,8 +68,11 @@ namespace Lemonade_Stand
         public void BuyLemons(Player player)
         {
             string response;
+            Console.Clear();
             Console.WriteLine("");
-            Console.WriteLine("Lemons are currently on sale! Check out these crazy deals!");   
+            player.GetInventory();
+            Console.WriteLine("");
+            Console.WriteLine("Lemons are currently on sale! Check out these crazy TART looking deals!");   
             Console.WriteLine("10 lemons for 0.64$.");
             Console.WriteLine("30 lemons for 2.16$.");
             Console.WriteLine("75 lemons for 4.19$.");
@@ -87,6 +87,7 @@ namespace Lemonade_Stand
                     player.Lemons += 10;
                     player.Money -= 0.64;
                     Console.WriteLine("Purchase was succesful! You will now return to the store menu. ");
+                    Console.ReadLine();
                     StartShopping(player);
                 }
                 else
@@ -103,6 +104,7 @@ namespace Lemonade_Stand
                     player.Lemons += 30;
                     player.Money -= 2.16;
                     Console.WriteLine("Purchase was succesful! You will now return to the store menu. ");
+                    Console.ReadLine();
                     StartShopping(player);
                 }
                 else
@@ -120,6 +122,7 @@ namespace Lemonade_Stand
                     player.Lemons += 75;
                     player.Money -= 4.19;
                     Console.WriteLine("Purchase was succesful! You will now return to the store menu. ");
+                    Console.ReadLine();
                     StartShopping(player);
                 }
                 else
@@ -145,6 +148,267 @@ namespace Lemonade_Stand
 
 
         }
+
+        public void BuyIce(Player player)
+        {
+            string response;
+            Console.Clear();
+            Console.WriteLine("");
+            player.GetInventory();
+            Console.WriteLine("");
+            Console.WriteLine("Ice is currently on sale! Check out these wacky shockingly HOT deals!");
+            Console.WriteLine("100 ice cubes for 0.88$.");
+            Console.WriteLine("250 ice cubes for 2.00$.");
+            Console.WriteLine("500 cubes for 3.66$.");
+            Console.WriteLine("");
+            Console.WriteLine(" Enter the number you would like to purchase, '100','250','500', or 'return'.");
+            response = Console.ReadLine().ToLower().Trim();
+
+            if (response == "100")
+            {
+                if (player.money > 0.88)
+                {
+                    player.Ice += 100;
+                    player.Money -= 0.88;
+                    Console.WriteLine("Purchase was succesful! You will now return to the store menu. ");
+                    Console.ReadLine();
+                    StartShopping(player);
+                }
+                else
+                {
+                    Console.WriteLine("You do not have the sufficient funds, alter amount or return.");
+                    BuyIce(player);
+                }
+
+            }
+            else if (response == "250")
+            {
+                if (player.money > 2.00)
+                {
+                    player.Ice += 250;
+                    player.Money -= 2.00;
+                    Console.WriteLine("Purchase was succesful! You will now return to the store menu. ");
+                    Console.ReadLine();
+                    StartShopping(player);
+                }
+                else
+                {
+                    Console.WriteLine("You do not have the sufficient funds, alter amount or return.");
+                    BuyIce(player);
+                }
+
+            }
+            else if (response == "500")
+            {
+
+                if (player.money > 3.66)
+                {
+                    player.Ice += 500;
+                    player.Money -= 3.66;
+                    Console.WriteLine("Purchase was succesful! You will now return to the store menu. ");
+                    Console.ReadLine();
+                    StartShopping(player);
+                }
+                else
+                {
+                    Console.WriteLine("You do not have the sufficient funds, alter amount or return.");
+                    BuyIce(player);
+                }
+
+
+            }
+            else if (response == "return")
+            {
+                StartShopping(player);
+
+            }
+            else
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Answer is invalid, read prompt and answer accordingly. Hit enter when ready");
+                Console.ReadLine();
+                BuyIce(player);
+            }
+
+
+        }
+
+        public void BuyCups(Player player)
+        {
+            string response;
+            Console.Clear();
+            Console.WriteLine("");
+            player.GetInventory();
+            Console.WriteLine("");
+            Console.WriteLine("Cups are currently on sale! Check out these super awesome DRINKABLE deals!");
+            Console.WriteLine("25 cups for 0.86$.");
+            Console.WriteLine("50 cups for 1.63$.");
+            Console.WriteLine("100 cups for 2.75$.");
+            Console.WriteLine("");
+            Console.WriteLine(" Enter the number you would like to purchase, '25','50','100', or 'return'.");
+            response = Console.ReadLine().ToLower().Trim();
+
+            if (response == "25")
+            {
+                if (player.money > 0.86)
+                {
+                    player.Cups += 25;
+                    player.Money -= 0.86;
+                    Console.WriteLine("Purchase was succesful! You will now return to the store menu. ");
+                    Console.ReadLine();
+                    StartShopping(player);
+                }
+                else
+                {
+                    Console.WriteLine("You do not have the sufficient funds, alter amount or return.");
+                    BuyCups(player);
+                }
+
+            }
+            else if (response == "50")
+            {
+                if (player.money > 1.63)
+                {
+                    player.Cups += 50;
+                    player.Money -= 1.63;
+                    Console.WriteLine("Purchase was succesful! You will now return to the store menu. ");
+                    Console.ReadLine();
+                    StartShopping(player);
+                }
+                else
+                {
+                    Console.WriteLine("You do not have the sufficient funds, alter amount or return.");
+                    BuyCups(player);
+                }
+
+            }
+            else if (response == "100")
+            {
+
+                if (player.money > 2.75)
+                {
+                    player.Cups += 100;
+                    player.Money -= 2.75;
+                    Console.WriteLine("Purchase was succesful! You will now return to the store menu. ");
+                    Console.ReadLine();
+                    StartShopping(player);
+                }
+                else
+                {
+                    Console.WriteLine("You do not have the sufficient funds, alter amount or return.");
+                    BuyCups(player);
+                }
+
+
+            }
+            else if (response == "return")
+            {
+                StartShopping(player);
+
+            }
+            else
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Answer is invalid, read prompt and answer accordingly. Hit enter when ready");
+                Console.ReadLine();
+                BuyCups(player);
+            }
+
+
+        }
+
+
+        public void BuySugar(Player player)
+        {
+            string response;
+            Console.Clear();
+            Console.WriteLine("");
+            player.GetInventory();
+            Console.WriteLine("");
+            Console.WriteLine("Sugar is currently on sale! Check out these SWEET and yummy deals!");
+            Console.WriteLine("8 cups of sugar for 0.51.");
+            Console.WriteLine("20 cups of sugar  for 1.64$.");
+            Console.WriteLine("48 cups of sugar for 3.28$.");
+            Console.WriteLine("");
+            Console.WriteLine(" Enter the number you would like to purchase, '8','20','48', or 'return'.");
+            response = Console.ReadLine().ToLower().Trim();
+
+            if (response == "8")
+            {
+                if (player.money > 0.51)
+                {
+                    player.Sugar += 8;
+                    player.Money -= 0.51;
+                    Console.WriteLine("Purchase was succesful! You will now return to the store menu. ");
+                    Console.ReadLine();
+                    StartShopping(player);
+                }
+                else
+                {
+                    Console.WriteLine("You do not have the sufficient funds, alter amount or return.");
+                    BuySugar(player);
+                }
+
+            }
+            else if (response == "20")
+            {
+                if (player.money > 1.64)
+                {
+                    player.Sugar += 20;
+                    player.Money -= 1.64;
+                    Console.WriteLine("Purchase was succesful! You will now return to the store menu. ");
+                    Console.ReadLine();
+                    StartShopping(player);
+                }
+                else
+                {
+                    Console.WriteLine("You do not have the sufficient funds, alter amount or return.");
+                    BuySugar(player);
+                }
+
+            }
+            else if (response == "48")
+            {
+
+                if (player.money > 3.28)
+                {
+                    player.Sugar += 48;
+                    player.Money -= 3.28;
+                    Console.WriteLine("Purchase was succesful! You will now return to the store menu. ");
+                    Console.ReadLine();
+                    StartShopping(player);
+                }
+                else
+                {
+                    Console.WriteLine("You do not have the sufficient funds, alter amount or return.");
+                    BuySugar(player);
+                }
+
+
+            }
+            else if (response == "return")
+            {
+                StartShopping(player);
+
+            }
+            else
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Answer is invalid, read prompt and answer accordingly. Hit enter when ready");
+                Console.ReadLine();
+                BuySugar(player);
+            }
+
+
+        }
+
+
+
+
+
+
+
+
 
 
 
